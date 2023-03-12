7b9ff706a2c6585b50d19b60cb7480823c6198b7
Nanihero <110215858+Nanihero@users.noreply.github.com>
2023-03-12 11:14:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,21 @@
-* {box-sizing: border-box}
+const form = document.getElementById('registration-form');
+const table = document.getElementById('entries').getElementsByTagName('tbody')[0];
 
-/* Add padding to containers */
-.container {
-  padding: 16px;
-}
-
-/* Full-width input fields */
-input[type=text], input[type=password] {
-  width: 100%;
-  padding: 15px;
-  margin: 5px 0 22px 0;
-  display: inline-block;
-  border: none;
-  background: #f1f1f1;
-}
-
-input[type=text]:focus, input[type=password]:focus {
-  background-color: #ddd;
-  outline: none;
-}
-
-/* Overwrite default styles of hr */
-hr {
-  border: 1px solid #f1f1f1;
-  margin-bottom: 25px;
-}
-
-/* Set a style for the submit/register button */
-.registerbtn {
-  background-color: #04AA6D;
-  color: white;
-  padding: 16px 20px;
-  margin: 8px 0;
-  border: none;
-  cursor: pointer;
-  width: 100%;
-  opacity: 0.9;
-}
-
-.registerbtn:hover {
-  opacity:1;
-}
-
-/* Add a blue text color to links */
-a {
-  color: dodgerblue;
-}
-
-/* Set a grey background color and center the text of the "sign in" section */
-.signin {
-  background-color: #f1f1f1;
-  text-align: center;
-}
+form.addEventListener('submit', function(event) {
+	event.preventDefault();
+	
+	const name = form.elements['name'].value;
+	const email = form.elements['email'].value;
+	const dob = form.elements['dob'].value;
+	
+	const newRow = table.insertRow();
+	const nameCell = newRow.insertCell();
+	const emailCell = newRow.insertCell();
+	const dobCell = newRow.insertCell();
+	
+	nameCell.innerHTML = name;
+	emailCell.innerHTML = email;
+	dobCell.innerHTML = dob;
+	
+	form.reset();
+});
